0a168b5b77bb70f7e3731c0f7bdca59a93db1e11
TsukumoOhira <m1m23a06@oit.ac.jp>
2023-07-03 19:52:06 +0900

    drawmonsterとtoStringの追加

--- app/src/main/java/semonster2/Player.java
+++ app/src/main/java/semonster2/Player.java
@@ -12,4 +12,20 @@ public class Player {
     this.randomNumberList = randomNumberList;
     this.name = playerName;
   }
+
+  public void drawMonsters() {
+    for (int i = 0; i < 5; i++) {
+      this.monsterDeck.add(new Monster(this.randomNumberList.pop(), this.randomNumberList.pop()));
+    }
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("Deck:" + this.name + "\n");
+    for (Monster m : this.monsterDeck) {
+      sb.append(m);
+    }
+    return sb.toString();
+  }
 }
---
+++ app/src/test/java/semonster2/PlayerTest.java
@@ -0,0 +1,7 @@
+package semonster2;
+
+import org.junit.jupiter.api.Test;
+
+public class PlayerTest {
+    
+}
