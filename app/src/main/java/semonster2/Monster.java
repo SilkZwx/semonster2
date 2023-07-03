5f4b008f5213e9e77f4915ede0bf00d618c8bb17
TsukumoOhira <m1m23a06@oit.ac.jp>
2023-07-03 10:46:24 +0900

    summonMonster追加テスト

--- app/src/main/java/semonster2/Monster.java
+++ app/src/main/java/semonster2/Monster.java
@@ -4,6 +4,11 @@ public class Monster {
     private String name;
     private int rare;
 
+    String summonsMonster(int x){
+        String monsters[] = {"スライム","サハギン","ドラゴン","デュラハン","シーサーペント"};
+        return monsters[x];
+    }
+
     @Override
     public String toString() {
         return "デュラハン:レア度[0]\n";
--- app/src/test/java/semonster2/MonsterTest.java
+++ app/src/test/java/semonster2/MonsterTest.java
@@ -8,4 +8,14 @@ public class MonsterTest {
         Monster classUnderTest = new Monster();
         assertNotNull(classUnderTest.toString(), "デュラハン:レア度[0]");
     }
+
+    @Test void testSummonsMonster() {
+        Monster classUnderTest = new Monster();
+        assertEquals(classUnderTest.summonsMonster(0), "スライム");
+        assertEquals(classUnderTest.summonsMonster(1), "サハギン");
+        assertEquals(classUnderTest.summonsMonster(2), "ドラゴン");
+        assertEquals(classUnderTest.summonsMonster(3), "デュラハン");
+        assertEquals(classUnderTest.summonsMonster(4), "シーサーペント");
+    }
+
 }
