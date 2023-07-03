8706c522751192dff459d1f23762295583de0c51
SilkZwx <silkspheresilky@gmail.com>
2023-07-03 10:03:43 +0900

    タイピスト1の開発

--- app/src/main/java/semonster2/App.java
+++ app/src/main/java/semonster2/App.java
@@ -5,7 +5,7 @@ package semonster2;
 
 public class App {
     public String getGreeting() {
-        return "Hello World!";
+        return "こんにちは SEMomster";
     }
 
     public static void main(String[] args) {
