cc2dd29c13e9636889cdf4bf394df157a09b679d
SilkZwx <silkspheresilky@gmail.com>
2023-07-03 19:39:13 +0900

    Playerクラスの追加

--- app/src/test/java/semonster2/MonsterTest.java
+++ app/src/test/java/semonster2/MonsterTest.java
@@ -4,18 +4,20 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class MonsterTest {
-    @Test void appHasAGreeting() {
-        Monster classUnderTest = new Monster(3, 0);
-        assertEquals(classUnderTest.toString(), "デュラハン:レア度[0]\n");
-    }
+  @Test
+  void appHasAGreeting() {
+    Monster classUnderTest = new Monster(3, 0);
+    assertEquals(classUnderTest.toString(), "デュラハン:レア度[0]\n");
+  }
 
-    @Test void testSummonsMonster() {
-        Monster classUnderTest = new Monster(3, 0);
-        assertEquals(classUnderTest.summonsMonster(0), "スライム");
-        assertEquals(classUnderTest.summonsMonster(1), "サハギン");
-        assertEquals(classUnderTest.summonsMonster(2), "ドラゴン");
-        assertEquals(classUnderTest.summonsMonster(3), "デュラハン");
-        assertEquals(classUnderTest.summonsMonster(4), "シーサーペント");
-    }
+  @Test
+  void testSummonsMonster() {
+    Monster classUnderTest = new Monster(3, 0);
+    assertEquals(classUnderTest.summonsMonster(0), "スライム");
+    assertEquals(classUnderTest.summonsMonster(1), "サハギン");
+    assertEquals(classUnderTest.summonsMonster(2), "ドラゴン");
+    assertEquals(classUnderTest.summonsMonster(3), "デュラハン");
+    assertEquals(classUnderTest.summonsMonster(4), "シーサーペント");
+  }
 
 }
