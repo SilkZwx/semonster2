3938e1ee9c5162a5acb057cb11b5c0e0be648fa8
m1m23a34 <m1m23a34@oit.ac.jp>
2023-07-06 21:03:43 +0900

    SeMonsterGame.javaの作成

---
+++ app/src/test/java/semonster2/SeMonsterGameTest.java
@@ -0,0 +1,11 @@
+package semonster2;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class SeMonsterGameTest {
+    @Test
+    
+}
